e92319332570d53bea9da764c4b1dfefaa647370
Brett Best <6104381+Brett-Best@users.noreply.github.com>
2025-11-24 21:53:51 +1100

    Update version to 3.27.7

--- license-plist.rb
+++ license-plist.rb
@@ -1,8 +1,8 @@
 class LicensePlist < Formula
   desc "A license list generator of all your dependencies for iOS applications"
   homepage ""
-  url "https://github.com/mono0926/LicensePlist/archive/3.27.2.tar.gz"
-  sha256 "8593c0673611ad0f904f3c14ccd0e5e88e508796e77db700cfda1035c2b5cc5c"
+  url "https://github.com/mono0926/LicensePlist/archive/3.27.7.tar.gz"
+  sha256 "6e5ad3b0d9779993d6948c8f61ce73693d4317b331dc62c6a0a1b866d1b46c42"
 
   def install
     system "make", "install", "PREFIX=#{prefix}"
